58ccda99c616be6b349adf9d559e0a0bcd3138a6
Grubîi Cristian <146663864+CristianGr233@users.noreply.github.com>
2023-12-23 16:15:30 +0200

    Update file_reading.py

--- file_reading.py
+++ file_reading.py
@@ -1,2 +1,11 @@
 import csv
+#with open('Groups and Subjects (FAF Hack) - Profesori.csv', mode ='r')as file:
+  #csvFile = csv.reader(file)
+  #for lines in csvFile:
+  #      for i in lines:
+#with open('Groups and Subjects (FAF Hack) - Grupe.csv',mode='r')as file1:
+#    grupe=csv.reader(file1)
 
+ #   for lines in grupe:
+ #       for element in lines:
+ #           print(element)
