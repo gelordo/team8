e3274511cfd1f13ac2aa391a165a0f8b2e9220f8
BurdujaAdrian <105036513+BurdujaAdrian@users.noreply.github.com>
2023-12-24 05:06:57 +0200

    Update to read from database file_reading.py

--- file_reading.py
+++ file_reading.py
@@ -1,5 +1,6 @@
 import csv
 import random
+import mysql.connector
 
 class Groups:
     def __init__(self,id,sp,lg,nr,list_of_subs_ids):
@@ -14,7 +15,6 @@ class Groups:
     
     def add_subs_classes(self,class_list):
         self.list_of_subs = []
-        #print(self.list_of_subs_ids)
         self.list_of_subs_ids = list(map(int, self.list_of_subs_ids.split(', ')))
 
         for id in self.list_of_subs_ids:
@@ -69,50 +69,109 @@ class per_hour:
         self.type =type
 #reading from the file
 
+# groups_list = []
+
+# with open('Groups and Subjects (FAF Hack) - Grupe.csv',mode='r')as file1:
+#     grupe=csv.reader(file1)
+#     for lines in grupe:
+#         try:
+#             groups_list.append(Groups(lines[0],lines[1],lines[2],lines[3],lines[4]))
+#         except Exception:
+#             continue
+
+
+# cab_list = []
+
+# with open('Groups and Subjects (FAF Hack) - Cabinete.csv')as file2:
+#     cabs = csv.reader(file2)
+#     for lines in cabs:
+#         try:
+#             cab_list.append(Cabinets(lines[0],lines[1],lines[2]))
+#         except :
+#             print("skipped:",lines)
+#             continue 
+
+# sub_list = []
+
+# with open('Groups and Subjects (FAF Hack) - Subiecte.csv')as file3:
+#     subs = csv.reader(file3)
+#     for lines in subs:
+#         try:
+#             sub_list.append(Subjects(lines[0],lines[1],lines[2],lines[3],lines[4],lines[5],lines[6],lines[7]))
+#         except:
+#             print('error at', lines)
+#             continue
+
+
+# prof_list = []
+
+# with open('Groups and Subjects (FAF Hack) - Profesori.csv')as file4:
+#     prof = csv.reader(file4)
+#     for lines in prof:
+#         try:
+#             prof_list.append(Professors(lines[0],lines[1],lines[2],lines[3],lines[4:-1]))
+#         except:
+#             continue
+
+conn = mysql.connector.connect(
+    host="sql11.freesqldatabase.com",
+    user="sql11672526",
+    password="QVkl1fXMYs",
+    database="sql11672526"
+)
+cursor = conn.cursor()
+
+
+cursor.execute("SELECT * FROM Grupe")
+rows = cursor.fetchall()
 groups_list = []
 
-with open('Groups and Subjects (FAF Hack) - Grupe.csv',mode='r')as file1:
-    grupe=csv.reader(file1)
-    for lines in grupe:
+for lines in rows:
         try:
             groups_list.append(Groups(lines[0],lines[1],lines[2],lines[3],lines[4]))
         except Exception:
             continue
 
 
+cursor.execute("SELECT * FROM Cabinete")
+rows = cursor.fetchall()
 cab_list = []
 
-with open('Groups and Subjects (FAF Hack) - Cabinete.csv')as file2:
-    cabs = csv.reader(file2)
-    for lines in cabs:
+for lines in rows:
         try:
             cab_list.append(Cabinets(lines[0],lines[1],lines[2]))
         except :
             print("skipped:",lines)
             continue 
 
+
+cursor.execute("SELECT * FROM Subiecte")
+rows = cursor.fetchall()
 sub_list = []
 
-with open('Groups and Subjects (FAF Hack) - Subiecte.csv')as file3:
-    subs = csv.reader(file3)
-    for lines in subs:
+for lines in rows:
         try:
             sub_list.append(Subjects(lines[0],lines[1],lines[2],lines[3],lines[4],lines[5],lines[6],lines[7]))
-        except:
-            print('error at', lines)
-            continue
+        except :
+            print("skipped:",lines)
+            continue 
 
-for i in range(len(groups_list)):
-    groups_list[i].add_subs_classes(sub_list)
 
+cursor.execute("SELECT * FROM Profesori")
+rows = cursor.fetchall()
 prof_list = []
 
-with open('Groups and Subjects (FAF Hack) - Profesori.csv')as file4:
-    prof = csv.reader(file4)
-    for lines in prof:
+for lines in rows:
         try:
             prof_list.append(Professors(lines[0],lines[1],lines[2],lines[3],lines[4:-1]))
-        except:
-            continue
+        except :
+            print("skipped:",lines)
+            continue 
 
+conn.close() 
+
+
+# for i in range(len(groups_list)):
+#     groups_list[i].add_subs_classes(sub_list)
+# uncomment when the database is updated
 
